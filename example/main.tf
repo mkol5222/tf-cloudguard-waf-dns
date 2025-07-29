923101c4f98b020576a369677098e4200e174295
Martin Koldovsky <mkoldov@gmail.com>
2025-07-29 08:49:44 +0200

    added example use

---
+++ example/main.tf
@@ -0,0 +1,15 @@
+module "wafdns" {
+    source = "git::https://github.com/mkol5222/tf-cloudguard-waf-dns.git//cloudguard-waf-dns?ref=main"
+    WAFKEY  = var.WAFKEY
+    WAFSECRET = var.WAFSECRET
+}
+
+variable "WAFKEY" {}
+variable "WAFSECRET" {}
+
+output "dns_cert_validation_cnames" {
+  value = module.wafdns.dns_cert_validation_cnames
+}
+output "dns_service_domains" {
+  value = module.wafdns.dns_service_domains
+}
